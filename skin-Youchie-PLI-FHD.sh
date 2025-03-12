08e641c36446707c3a90b9d29e17f81e40168c6a
emil-nabil333 <elbrinselomda@yahoo.com>
2025-03-12 19:01:54 +0200

    Add files via upload

---
+++ skin-Youchie-PLI-FHD.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/emilnabil/download-plugins/refs/heads/main/apod/skin-Youchie-PLI-FHD.sh -O - | /bin/sh
+##
+###########################################
+###########################################
+
+# My config script #
+PLUGIN_TAR="skin-Youchie-PLI-FHD.tar.gz"
+MY_URL="https://raw.githubusercontent.com/emilnabil/download-plugins/refs/heads/main/apod"
+
+######################################################################################
+MY_EM='============================================================================================================'
+
+# Remove Old Plugin
+echo "   >>>>   Removing old version..."
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/apod
+
+echo "============================================================================================================================"
+echo " DOWNLOAD AND INSTALL PLUGIN "
+echo "   Installing plugin, please wait..."
+
+cd /tmp || exit 1
+set -e
+
+wget "$MY_URL/$PLUGIN_TAR"
+sleep 1
+tar xzvpf "/tmp/$PLUGIN_TAR" -C /
+rm -rf "/tmp/$PLUGIN_TAR"
+
+echo "================================="
+set +e
+
+if [ $? -eq 0 ]; then
+    echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
+fi
+
+echo "********************************************************************************"
+echo "   UPLOADED BY  >>>>   EMIL_NABIL " 
+sleep 4                        
+echo "$MY_EM"
+echo "**********************************************************************************"
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
