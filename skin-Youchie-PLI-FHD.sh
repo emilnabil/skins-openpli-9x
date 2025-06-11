7777c4138f3e01b6af2f3e41ccea86b8711f9e3e
emil-nabil333 <elbrinselomda@yahoo.com>
2025-06-11 23:18:03 +0300

    Add files via upload

--- skin-Youchie-PLI-FHD.sh
+++ skin-Youchie-PLI-FHD.sh
@@ -2,39 +2,30 @@
 ########
 ## Command=wget https://raw.githubusercontent.com/emilnabil/skins-openpli-9x/refs/heads/main/skin-Youchie-PLI-FHD.sh -O - | /bin/sh
 ##
-echo "Removing previous version of Youchie-FHD..."
-sleep 2
 
-# Check if the directory exists before removing it
+echo "Removing previous version of Youchie-FHD..."
+
 if [ -d /usr/share/enigma2/Youchie-PLI-FHD ]; then
-    rm -rf /usr/share/enigma2/Youchie-PLI-FHD
+    rm -rf /usr/share/enigma2/Youchie-PLI-FHD > /dev/null 2>&1
     rm -rf /usr/lib/enigma2/python/Plugins/Extensions/YouchieSettings > /dev/null 2>&1
-    
-    echo 'Package removed.'
+    echo "Package removed."
 else
     echo "You do not have previous version"
 fi
 
 echo ""
-opkg install enigma2-plugin-extensions-bitrate enigma2-plugin-extensions-oaweather
-opkg install curl
-sleep 2
+opkg install enigma2-plugin-extensions-bitrate enigma2-plugin-extensions-oaweather > /dev/null 2>&1
+opkg install curl > /dev/null 2>&1
 
-# Download and extract the package
 cd /tmp || exit
-curl -k -Lbk -m 55532 -m 555104 "https://github.com/emilnabil/skins-openpli-9x/raw/refs/heads/main/skin-Youchie-PLI-FHD.tar.gz" -o /tmp/skin-Youchie-PLI-FHD.tar.gz
-sleep 1
+curl -s -k -L "https://dreambox4u.com/emilnabil237/skins/obh/skin-Youchie-OBH-FHD.tar.gz" -o /tmp/skin-Youchie-OBH-FHD.tar.gz
 echo "Installing ...."
-tar -xzf /tmp/skin-Youchie-PLI-FHD.tar.gz -C /
+tar -xzf /tmp/skin-Youchie-OBH-FHD.tar.gz -C / > /dev/null 2>&1
 echo ""
 echo ""
-sleep 1
-rm -f /tmp/skin-Youchie-PLI-FHD.tar.gz
+rm -f /tmp/skin-Youchie-OBH-FHD.tar.gz > /dev/null 2>&1
 
-exit 
-
-
-
+exit
 
 
 
